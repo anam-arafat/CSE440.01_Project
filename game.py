37add99b36af95038a18313c9ffa021da06b6645
Rowshan29 <khandker.puspita@northsouth.edu>
2024-09-28 18:25:07 +0600

    print_board Method

--- game.py
+++ game.py
@@ -2,3 +2,7 @@ class TicTacToe:
     def __init__(self):
         self.board = [' ' for _ in range(9)]  # 3x3 board in a 1D list
         self.current_winner = None  # Tracks the winner
+        def print_board(self):
+        # Print the game board
+        for row in [self.board[i * 3:(i + 1) * 3] for i in range(3)]:
+            print('| ' + ' | '.join(row) + ' |')
