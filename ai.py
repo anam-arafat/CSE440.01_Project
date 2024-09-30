fcd9ea230511635ded4adeef473b5c36ce86c627
anam-arafat <anamarafat@gmail.com>
2024-09-30 19:25:10 +0600

    Refined Minimax

--- ai.py
+++ ai.py
@@ -10,31 +10,31 @@ def minimax(self, state, depth, alpha, beta, is_maximizing):
     elif state.current_winner == 'O':
         return 1 if self.letter == 'O' else -1
     elif state.empty_squares() == False:
-        return 0  # It's a tie
+        return 0
 
     if is_maximizing:
-        max_eval = -math.inf
-        for move in state.available_moves():    
+        best_value = -math.inf
+        for move in state.available_moves():
             state.make_move(move, self.letter)
             sim_score = self.minimax(state, depth + 1, alpha, beta, False)
             state.board[move] = ' '
             state.current_winner = None
-            max_eval = max(max_eval, sim_score)
+            best_value = max(best_value, sim_score)
             alpha = max(alpha, sim_score)
             if beta <= alpha:
                 break
-        return max_eval
+        return best_value
+    
     else:
-        min_eval = math.inf
+        best_value = math.inf
         opponent = 'O' if self.letter == 'X' else 'X'
         for move in state.available_moves():
             state.make_move(move, opponent)
             sim_score = self.minimax(state, depth + 1, alpha, beta, True)
             state.board[move] = ' '
             state.current_winner = None
-            min_eval = min(min_eval, sim_score)
+            best_value = min(best_value, sim_score)
             beta = min(beta, sim_score)
             if beta <= alpha:
                 break
-        return min_eval
-
+        return best_value
